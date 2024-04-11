af2f1a16f72d6c3a91e4ecd668a815f3320ec7b7
r-kag <rubykagaragwa@gmail.com>
2024-04-11 14:56:10 -0400

    Beta Demo

--- app.js
+++ app.js
@@ -53,12 +53,61 @@ const startHoles = [
     yellowHole, '', '', '', '', '', '', '', '', '', yellowHole, '', '', '', '', '', '', '', '', '', blueHole,
     yellowHole, yellowHole, yellowHole, yellowHole, yellowHole, yellowHole, yellowHole, yellowHole, yellowHole, yellowHole, yellowHole, blueHole, blueHole, blueHole, blueHole, blueHole, blueHole, blueHole, blueHole, blueHole, blueHole,]
 
+const rPin = []
+const gPin = []
+const bPin = []
+const yPin = []
+const homespaces = [[], [], [], []]
 //YT 15:51
 function createBoard() {
     startPieces.forEach((startPiece, i) => {
         const square = document.createElement('div')
         square.classList.add('square')
         square.innerHTML = startPiece;
+        switch (startPiece) {
+            case yellowPin:
+                yPin.push(square)
+                break;
+            case redPin:
+                rPin.push(square)
+                break;
+            case bluePin:
+                bPin.push(square)
+                break;
+            case greenPin:
+                gPin.push(square)
+                break;
+        }
+        switch (i) {
+            //square id's for red home
+            case 211:
+            case 212:
+            case 213:
+            case 214:
+                homespaces[0].push(square);
+                break;
+            //square id's for green home
+            case 31:
+            case 52:
+            case 73:
+            case 94:
+                homespaces[1].push(square);
+                break;
+            //square id's for blue home
+            case 226:
+            case 227:
+            case 228:
+            case 229:
+                homespaces[2].push(square);
+                break;
+            //square id's for yellow home
+            case 346:
+            case 367:
+            case 388:
+            case 409:
+                homespaces[3].push(square);
+                break;
+        }
         //console.log(square)
         // drag and drop functionality chapter 15 24:10
         //square.firstChild && console.log(square.firstChild.getAttribute('IsAPin'))
@@ -106,6 +155,35 @@ function dragOver(e) {
 }
 
 function dragDrop(e) {
-    e.stopPropagation()
-    e.target.append(draggedElement)
+    console.log(e.target.classList[0])
+    if (e.target.classList[0] == 'square') {
+        e.stopPropagation()
+        e.target.append(draggedElement)
+    }
+    var board = document.getElementById("Game-Winner");
+    if (homespaces[0][0].firstChild &&
+        homespaces[0][1].firstChild &&
+        homespaces[0][2].firstChild &&
+        homespaces[0][3].firstChild) {
+        board.appendChild(document.createTextNode("Red Wins!"));
+    } else if (
+        homespaces[1][0].firstChild &&
+        homespaces[1][1].firstChild &&
+        homespaces[1][2].firstChild &&
+        homespaces[1][3].firstChild) {
+        board.appendChild(document.createTextNode("Green Wins!"));
+    } else if (
+        homespaces[2][0].firstChild &&
+        homespaces[2][1].firstChild &&
+        homespaces[2][2].firstChild &&
+        homespaces[2][3].firstChild) {
+        board.appendChild(document.createTextNode("Blue Wins!"));
+    } else if (
+        homespaces[3][0].firstChild &&
+        homespaces[3][1].firstChild &&
+        homespaces[3][2].firstChild &&
+        homespaces[3][3].firstChild) {
+        board.appendChild(document.createTextNode("Yellow Wins!"));
+    }
 }
+
--- cards.js
+++ cards.js
@@ -1,54 +1,126 @@
-const SUITS = ["♠", "♣", "♥", "♦"]
-const VALUES = [
-    "A",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10",
-    "J",
-    "Q",
-    "K"
-    "JKR"
+const Deck = [
+    ['A', "♠", false], ['A', "♣", false], ['A', "♥", false], ['A', "♦", false], ['2', "♠", false], ['2', "♣", false], ['2', "♥", false], ['2', "♦", false],
+    ['3', "♠", false], ['3', "♣", false], ['3', "♥", false], ['3', "♦", false], ['4', "♠", false], ['4', "♣", false], ['4', "♥", false], ['4', "♦", false],
+    ['5', "♠", false], ['5', "♣", false], ['5', "♥", false], ['5', "♦", false], ['6', "♠", false], ['6', "♣", false], ['6', "♥", false], ['6', "♦", false],
+    ['7', "♠", false], ['7', "♣", false], ['7', "♥", false], ['7', "♦", false], ['8', "♠", false], ['8', "♣", false], ['8', "♥", false], ['8', "♦", false],
+    ['9', "♠", false], ['9', "♣", false], ['9', "♥", false], ['9', "♦", false], ['10', "♠", false], ['10', "♣", false], ['10', "♥", false], ['10', "♦", false],
+    ['J', "♠", false], ['J', "♣", false], ['J', "♥", false], ['J', "♦", false], ['Q', "♠", false], ['Q', "♣", false], ['Q', "♥", false], ['Q', "♦", false],
+    ['K', "♠", false], ['K', "♣", false], ['K', "♥", false], ['K', "♦", false], ['JKR', "♠", false], ['JKR', "♦", false]]
 
-]
-const Round1Deck = [
-    { 'A', "♠"}, { 'A', "♣"}, { 'A', "♥"}, { 'A', "♦"}, { '2', "♠"}, { '2', "♣"}, { '2', "♥"}, { '2', "♦"},
-    { '3', "♠"}, { '3', "♣"}, { '3', "♥"}, { '3', "♦"}, { '4', "♠"}, { '4', "♣"}, { '4', "♥"}, { '4', "♦"},
-    { '5', "♠"}, { '5', "♣"}, { '5', "♥"}, { '5', "♦"}, { '6', "♠"}, { '6', "♣"}, { '6', "♥"}, { '6', "♦"},
-    { '7', "♠"}, { '7', "♣"}, { '7', "♥"}, { '7', "♦"}, { '8', "♠"}, { '8', "♣"}, { '8', "♥"}, { '8', "♦"},
-    { '9', "♠"}, { '9', "♣"}, { '9', "♥"}, { '9', "♦"}, { '10', "♠"}, { '10', "♣"}, { '10', "♥"}, { '10', "♦"},
-    { 'J', "♠"}, { 'J', "♣"}, { 'J', "♥"}, { 'J', "♦"}, { 'Q', "♠"}, { 'Q', "♣"}, { 'Q', "♥"}, { 'Q', "♦"},
-    { 'K', "♠"}, { 'K', "♣"}, { 'K', "♥"}, { 'K', "♦"}, { 'JKR', "♦"}, { 'JKR', "♦"},]
-const Round2Deck = [
-    { 'A', "♠"}, { 'A', "♣"}, { 'A', "♥"}, { 'A', "♦"}, { '2', "♠"}, { '2', "♣"}, { '2', "♥"}, { '2', "♦"},
-    { '3', "♠"}, { '3', "♣"}, { '3', "♥"}, { '3', "♦"}, { '4', "♠"}, { '4', "♣"}, { '4', "♥"}, { '4', "♦"},
-    { '5', "♠"}, { '5', "♣"}, { '5', "♥"}, { '5', "♦"}, { '6', "♠"}, { '6', "♣"}, { '6', "♥"}, { '6', "♦"},
-    { '7', "♠"}, { '7', "♣"}, { '7', "♥"}, { '7', "♦"}, { '8', "♠"}, { '8', "♣"}, { '8', "♥"}, { '8', "♦"},
-    { '9', "♠"}, { '9', "♣"}, { '9', "♥"}, { '9', "♦"}, { '10', "♠"}, { '10', "♣"}, { '10', "♥"}, { '10', "♦"},
-    { 'J', "♠"}, { 'J', "♣"}, { 'J', "♥"}, { 'J', "♦"}, { 'Q', "♠"}, { 'Q', "♣"}, { 'Q', "♥"}, { 'Q', "♦"},
-    { 'K', "♠"}, { 'K', "♣"}, { 'K', "♥"}, { 'K', "♦"}, { 'JKR', "♦"}, { 'JKR', "♦"},]
+function resetDeck() {
+    for (c in Deck) {
+        Deck[c][2] = false;
+    }
+}
+resetDeck();
 
-const Round1Hands = { Player1: [], Player2: [] }
+const Hands = [[], [], [], []]
 
 
-class Deck {
-    consturctor(cards) {
-        this.cards = cards
+var roundCounter = 0;
+
+function getRandomCard() {
+    r = Math.floor(Math.random() * 50) % 54;
+    safety = 0;
+    while (Deck[r % 54][2] && safety < 54) {
+        r++;
+        safety++;
+    }
+    Deck[r % 54][2] = true;
+    return Deck[r % 54]
+}
+
+function shuffleCards() {
+    for (i in Hands) {
+        Hands[i] = [];
+    }
+
+    currentDealer = (Math.floor(roundCounter / 3) % 4);
+    for (i = 0; i < 4; i++) {
+        for (j = 0; j < 4; j++) {
+            Hands[i].push(getRandomCard())
+        }
+    }
+
+    //TODO: check if it's the first round then deal 1 more card to each player
+    if (roundCounter % 3 == 0) {
+        for (i = 0; i < 4; i++) {
+            Hands[i].push(getRandomCard())
+        }
+    }
+
+    //TODO: check if last(third) round then deal 2 more to dealer
+    if (roundCounter % 3 == 2) {
+        for (i = 0; i < 2; i++) {
+            Hands[currentDealer].push(getRandomCard())
+        }
+    }
+
+    if (roundCounter % 3 == 2)
+        resetDeck()
+
+}
+
+
+function showHand(player) {
+    var display = null;
+    var temp = null;
+    display = document.getElementById("card-display")
+    display.replaceChildren()
+    var whichHand = null;
+    switch (player) {
+        case "red":
+            whichHand = 0;
+            break;
+        case "green":
+            whichHand = 1;
+            break;
+        case "blue":
+            whichHand = 2;
+            break;
+        case "yellow":
+            whichHand = 3;
+            break;
+    }
+    display = document.getElementById("card-display");
+    temp = document.getElementById("cardTemplate");
+    for (x in Hands[whichHand]) {
+        display.appendChild(temp.cloneNode(true));
+        display.lastChild.appendChild(document.createElement("p"))
+        display.lastChild.lastChild.appendChild(document.createTextNode(Hands[whichHand][x][0]))
+        display.lastChild.lastChild.style.position = 'relative';
+        display.lastChild.lastChild.style.top = '0';
+        display.lastChild.lastChild.style.right = '0';
+        display.lastChild.appendChild(document.createElement("p"))
+        display.lastChild.lastChild.appendChild(document.createTextNode(Hands[whichHand][x][1]))
+        display.lastChild.style.display = "inline-block";
+        display.lastChild.addEventListener("click", function (e) {
+            if (e.target.classList[0] != 'card')
+                dropCard(e.target.parentNode, whichHand);
+            else
+                dropCard(e.target, whichHand);
+        });
     }
 }
 
-class Card {
-    constructor(suit, value) {
-        this.suit = suit
-        this.value = value
+function dropCard(e, h) {
+    var suit = e.lastChild.textContent;
+    var num = e.firstChild.textContent;
+    document.getElementById("card-display").removeChild(e);
+    for (c in Hands[h]) {
+        if (Hands[h][c][0] == num && Hands[h][c][1] == suit) {
+            Hands[h].splice(c, 1);
+        }
     }
+    roundChange()
 }
 
-function freshDeck() {
-    return SUITS.flatmap
+function roundChange() {
+    for (i = 0; i < 4; i++) {
+        if (Hands[i].length > 0) {
+            return;
+        }
+    }
+    roundCounter++;
+    shuffleCards();
 }
